1946a86f4cd1068252392f5a6d2ff2bfcc62be18
augustok99 <carlosaugustox6@gmail.com>
2026-01-24 17:37:58 -0400

    feat:titulo mobile 2

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,17 +4,21 @@ import { CodeXml } from "lucide-react";
 
 const Hero = () => {
   return (
-    <div className="mt-12 w-[25rem] md:w-full justify-center flex">
+    <div className="mt-12 md:w-full justify-center flex">
       {/* COLUNA DIREITA */}
       <motion.div
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="flex flex-col gap-2 items-center px-10 md:pl-10"
+        className="flex flex-col text-center gap-2 items-center md:pl-10 px-10"
       >
-        <h1 className="md:text-6xl text-4xl italic font-playfair leading-tight pt-16 text-center md:text-center">
+        <h1
+          // quebrar linha com hyphens
+          style={{ hyphens: "auto", WebkitHyphens: "auto", msHyphens: "auto" }}
+          className="md:text-6xl text-4xl italic font-playfair leading-tight pt-16 md:text-center"
+        >
           <span className="block text-[#F2F2F2]">
-            Transformo <span className=" text-[#10b981]">ideias </span>
+            Transformo <span className="text-[#10b981]">ideias </span>
             em{" "}
             <span className=" text-[#10b981]">
               soluções <br /> digitais{" "}
